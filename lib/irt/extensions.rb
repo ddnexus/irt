adf3b8add0460cf51e9656df24e18e6425fab9e1
Domizio Demichelis <dd.nexus@gmail.com>
2010-12-13 23:38:42 -0400

    added Kernel#capture

--- lib/irt/extensions.rb
+++ lib/irt/extensions.rb
@@ -1,3 +1,17 @@
+require 'stringio'
+module Kernel
+
+  def capture
+    out = StringIO.new
+    $stdout = out
+    yield
+    return out.string
+  ensure
+    $stdout = STDOUT
+  end
+
+end
+
 class Object
   def object_methods
     methods - self.class.methods
