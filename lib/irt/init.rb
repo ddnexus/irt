93ba1d2ef94909a6496d1c6a4db7c089216589b7
Domizio Demichelis <dd.nexus@gmail.com>
2011-01-19 22:14:54 -0400

    fix for old version of irb (conf[:AT_EXIT] == nil)

--- lib/irt/init.rb
+++ lib/irt/init.rb
@@ -1,4 +1,5 @@
 module IRB #:nodoc:
+  conf[:AP_NAME] = 'irt'
   conf[:PROMPT][:IRT] = { :PROMPT_I => "%02n >> ",
                           :PROMPT_S => '   "> ',
                           :PROMPT_C => "%02n ?> ",
@@ -7,10 +8,10 @@ module IRB #:nodoc:
   conf[:PROMPT_MODE] = :IRT
   conf[:ECHO] = false
   conf[:VERBOSE] = false
-  conf[:AT_EXIT] << proc{IRT::Directives.test_summary}
-  conf[:AP_NAME] = 'irt'
   conf[:SAVE_HISTORY] = 100
   conf[:HISTORY_FILE] = File.expand_path '~/.irt-history'
+  conf[:AT_EXIT] ||= []
+  conf[:AT_EXIT] << proc{IRT::Directives.test_summary}
   conf[:AT_EXIT] << proc{ print "\e[0m" if Colorer.color? } # reset colors
 end
 
