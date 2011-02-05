2ec83dd96e24c8262dcdbce042f1e0926f0b44fc
Domizio Demichelis <dd.nexus@gmail.com>
2011-02-05 12:09:46 -0400

    little refactory of IRT::RubyVersion

--- lib/irt/ruby_version.rb
+++ lib/irt/ruby_version.rb
@@ -1,20 +1,20 @@
 module IRT
   module RubyVersion
 
-    extend self
-
     class Version < Array
       include Comparable
 
-       def initialize(version=RUBY_VERSION)
-         replace version.split('.').map(&:to_i)
-       end
+      def initialize(version)
+        replace version.split('.').map(&:to_i)
+      end
     end
+
+    extend self
 
     [:>, :>=, :<, :<=, :==, :between?].each do |m|
       define_method(m) do |*args|
         vers = args.map{|a| Version.new(a)}
-        Version.new.send m, *vers
+        Version.new(RUBY_VERSION).send m, *vers
       end
     end
 
